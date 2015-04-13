0f9632eccb814873d05629e3c111d592bcedfd87
grekazabr <Denys.Koval2012@gmail.com>
2015-04-13 20:42:52 +0300

    1

--- Helloworld.java
+++ Helloworld.java
@@ -1,8 +1,10 @@
 import java.io.File;
-
+import java.util.Scanner;
 public class Helloworld {
 	public static void main(String[] args){
-		File myFolder = new File("C:/Program Files");
+		Scanner in = new Scanner(System. in);
+		String path = in.nextLine();
+		File myFolder = new File(path);
 		File[] files = myFolder.listFiles();
 		for(int i = 0; i < files.length; i++)
 		{
